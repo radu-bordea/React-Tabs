4e86ae1da97faff3daee54e457982a9083fbdc17
radu-bordea <radu.bordea.dev@gmail.com>
2023-11-21 09:53:57 +0100

    create Jobinfo and duties components and fetching the jobs from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJbs] = useState(true);
+  // current item
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJbs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="jobs-center">
+       {/* button container */}
+       {/* job info */}
+       <JobInfo jobs={jobs} />
+    </section>
+  );
 };
 export default App;
